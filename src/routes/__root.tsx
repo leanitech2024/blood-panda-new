4a4de4756aa051ce5b822eed86a7374fdf2e72b8
Rajeev K Rao <rajeevraok@gmail.com>
2026-09-04 11:33:04 +0530

    google tag and facebook domain verification metatags added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -74,9 +74,35 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WTWB4NNF');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Facebook Domain Verification */}
+        <meta
+          name="facebook-domain-verification"
+          content="xx2ctipjz8rsccq142sa2qarz4rgaz"
+        />
         <HeadContent />
       </head>
       <body className="font-sans antialiased [wrap:anywhere] selection:bg-destructive/10 selection:text-destructive">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WTWB4NNF"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider defaultTheme="light" storageKey="theme">
           <PostHogProvider>
             <TooltipProvider>
